f9d9b0dd5387b890898b3c1609324160554c5e74
Venky <venkateshpamidi7@gmail.com>
2022-06-19 12:38:58 +0530

    initial commit

---
+++ 01_Basics/hello.js
@@ -0,0 +1,32 @@
+console.log("Hey!!!!!");
+
+//Texts - case sensitive
+
+// var smiley = "\U"
+
+/*    comments    */
+
+// Literals
+
+/* 22222
+22.325
+"string"
+'string'
+true
+false
+null */
+
+//Reserved Keywords
+
+/* enum -> future reserved keyword
+implements
+interface
+package
+private
+protected
+public
+more are there -- mdn */
+
+//unicode
+
+//semicolons
